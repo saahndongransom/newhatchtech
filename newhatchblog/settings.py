fd0dc48b052663c168cd6976c399b4d93cf50da0
username <saahndongransom@gmail.com>
2023-01-08 13:43:28 +0100

    add txt

--- newhatchblog/settings.py
+++ newhatchblog/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'nblog',
-    'ckeditor',
+    #'ckeditor',
     'paypal.standard.ipn',
     'crispy_forms',
     'tinymce',
